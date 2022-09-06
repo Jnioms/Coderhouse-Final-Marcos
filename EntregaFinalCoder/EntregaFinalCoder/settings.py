c4b2c011c8e5e10dbdc943cbc4cff4fb816f7a17
Jnioms <jnioms@hotmail.com>
2022-09-06 19:56:03 -0300

    v0.1.0, ya casi listo para deploy

--- EntregaFinalCoder/EntregaFinalCoder/settings.py
+++ EntregaFinalCoder/EntregaFinalCoder/settings.py
@@ -16,19 +16,18 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(path.join(r'C:\Users\juanm\Desktop\Proyectos\Python Coderhouse\Django Coder Final', '.env'))
+load_dotenv(path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = getenv('SECRET_KEY')
-print(getenv('EMAIL_HOST'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.0.193','matehost.com.ar']
+ALLOWED_HOSTS = ['127.0.0.1','192.168.0.193']
 
 
 # Application definition
